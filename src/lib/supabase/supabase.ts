aeca4f3cb804863d1c36f791bf03ac90a87354cf
Andrew O <aokeefe31@gmail.com>
2025-12-05 14:19:57 -0500

    add: server.ts for SSR + restore browser client — sync Vercel

---
+++ src/lib/supabase/supabase.ts
@@ -0,0 +1,12 @@
+// src/lib/supabase/server.ts
+import { createServerClient } from '@supabase/ssr'
+import { cookies } from 'next/headers'
+
+export const createServerSupabaseClient = () =>
+  createServerClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+      cookies
+    }
+  )
