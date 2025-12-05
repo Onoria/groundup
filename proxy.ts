c0a9baab260ff53523d4180a878210e3938b04f7
Andrew O <aokeefe31@gmail.com>
2025-12-05 18:32:00 -0500

    fix: final Clerk v5.5+ middleware – remove await + .protect() call

--- proxy.ts
+++ proxy.ts
@@ -6,8 +6,8 @@ const isProtectedRoute = createRouteMatcher([
   '/team(.*)',
 ])
 
-export default clerkMiddleware(async (auth, req) => {
-  if (isProtectedRoute(req)) await auth().protect()
+export default clerkMiddleware((auth, req) => {
+  if (isProtectedRoute(req)) auth().protect
 })
 
 export const config = {
